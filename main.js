89d431e298747f1a9f277442a33301bca856d621
Tom Scharstein <trscharstein@gmail.com>
2019-03-16 22:01:47 -0700

    Adding new map, folder mapTypeID terrain

--- main.js
+++ main.js
@@ -14,14 +14,14 @@ Vue.component('griderator', {
     template: `
         <div class="board">
             <div class="grid" :style="getGridStyle()">
-                <img v-for="cell in grids" style="width:20px; height:20px;" :key="cell.name" :src="'valucremap/' + cell.name" />
+                <img v-for="cell in grids" style="width:20px; height:20px;" :key="cell.name" :src="'terrain/' + cell.name" />
             </div>
         </div>
     `,
     data: () => ({
         grids: [],
         tileCounts: [],
-        max: 4,
+        max: 5,
     }),
     computed: {
         allTiles() {
@@ -72,50 +72,6 @@ Vue.component('griderator', {
             })
             console.log(this.grids)
         },
-        legacyConstructGrids() {
-            this.boards.forEach(board => {
-                board.zoomLevel = this.findZoomLevel(board.width / this.opts.size);
-                let mirror = [];
-                for (let i = 0; i < board.tilecount; i++) {
-                    let column = i % board.tilerows;
-                    let row = (i >= board.tilerows) ? Math.floor(i / board.tilerows) : 0;
-                    // let x1 = (column * this.opts.size) + board.pos[0];
-                    // let y1 = (row * this.opts.size) * -1 - board.pos[1];
-                    // if (y1 === -0)
-                    //     y1 = 0;
-                    // let x2 = (+x1) + (+this.opts.size);
-                    // let y2 = y1 - this.opts.size;
-                    // let quad = null;
-                    // if (column + 1 > (board.tilerows / 2)) {
-                    //     if (row + 1 > (board.tilerows / 2)) {
-                    //         quad = 3;
-                    //     } else {
-                    //         quad = 1;
-                    //     }
-                    // } else {
-                    //     if (row + 1 > (board.tilerows / 2)) {
-                    //         quad = 2;
-                    //     } else {
-                    //         quad = 0;
-                    //     }
-                    // }
-                    // mirror.push({
-                    //     name: null,
-                    //     x: column,
-                    //     y: row,
-                    //     z: board.zoomLevel,
-                    //     quadrant: quad,
-                    //     exists: false,
-                    //     selected: false,
-                    //     hover: false,
-                    //     rect: [x1, y1, x2, y2],
-                    // });
-                    // console.log(`calculated zoom: ${board.zoomLevel}, tile ${i} at column ${column}, row ${row} has rect: [${x1}, ${y1}, ${x2}, ${y2}]`)
-                }
-                // board.grid = mirror;
-            });
-            // console.log(this.boards);
-        }
     }
 })
 
