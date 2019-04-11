2226af02ae56f43b25683af0d7cd1613832d0c37
Tom Scharstein <trscharstein@gmail.com>
2019-04-10 19:14:04 -0700

    Updating /default

--- main.js
+++ main.js
@@ -14,14 +14,14 @@ Vue.component('griderator', {
     template: `
         <div class="board">
             <div v-for="(grid, key) in grids" :key="key" class="grid" :style="getGridStyle(grid)">
-                <img v-for="(cell,index) in grid" :key="index" style="width:20px; height:20px;" :key="cell.name" :src="'default/' + cell.name" />
+                <img v-for="(cell,index) in grid" :key="index" style="width:20px; height:20px;" :key="cell.name" :src="'terrain/' + cell.name" />
             </div>
         </div>
     `,
     data: () => ({
         grids: [],
         tileCounts: [],
-        max: 5,
+        max: 7,
     }),
     computed: {
         allTiles() {
@@ -38,9 +38,11 @@ Vue.component('griderator', {
             console.log(cols)
             return `
                 display: grid;
+                margin: 1rem;
+                width: ${cols * 20}px;
                 grid-template-columns: repeat(${cols}, 1fr);
-                border: 2px solid red;
-            `
+                `
+                // border: 2px solid red;
         },
         constructTileCounts(max) {
             let mirror = [];
@@ -66,13 +68,37 @@ Vue.component('griderator', {
                     const x = i % tilerows;
                     const y = (i >= tilerows) ? Math.floor(i / tilerows) : 0;
                     let name = null;
-                    if (count.z >= 5) {
+                    if (count.z == 5) {
                         let direction = null;
                         if (x >= 17)
                             direction = true;
                         else
                             direction = false;
                         name = `${count.z}/${(direction) ? 'B' : 'A'}/${count.z}-${x}-${y}.png`
+                    } else if (count.z == 6) { 
+                        if (x <= 13) {
+                            name = `${count.z}/1/${count.z}-${x}-${y}.png`
+                        } else {
+                            let finder = Math.floor(x / 13) + 1;
+                            let modulo = x % 13;
+                            name = `${count.z}/${finder}/${count.z}-${x}-${y}.png`
+                            if ((x == 26) || (x == 39) || (x == 52)) {
+                                name = `${count.z}/${finder - 1}/${count.z}-${x}-${y}.png`
+                            }
+                            if (i < 100) {
+                                console.log(name)
+                            }
+                        }
+                    } else if (count.z == 7) {
+                        if (x < 7) {
+                            name = `${count.z}/1/${count.z}-${x}-${y}.png`
+                        } else {
+                            let finder = Math.floor(x / 7) + 1;
+                            name = `${count.z}/${finder}/${count.z}-${x}-${y}.png`
+                            if (i < 100) {
+                                console.log(`${count.z}/${finder}/${count.z}-${x}-${y}.png`)
+                            }
+                        }
                     } else {
                         name = `${count.z}/${count.z}-${x}-${y}.png`
                     }
